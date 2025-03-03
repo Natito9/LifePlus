9ef9c5299886d7e410216140a18ff4f006dc70b0
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-03-03 21:00:55 +0100

    Add more styles to LandingPage and change the classNames to camelCase

--- src/routes/shared/LandingPage/LandingPage.jsx
+++ src/routes/shared/LandingPage/LandingPage.jsx
@@ -4,14 +4,14 @@ import bannerSvg from "../../../assets/images/banner.svg";
 
 function LandingPage() {
   return (
-    <div>
-        <img src={bannerSvg} alt="banner" />
-      <h2 className='bold-text'>Welcome to</h2>
-      <h1 className='bold-text'>life +</h1>
-      <h2 className='medium-text'>Who are you?</h2>
-      <button>Patient</button>
-      <button>Healthcare provider</button>
-      <p>By signing  up or logging in, I accept the apps <em>Terms of service</em> and <em> Privacy policy</em></p>
+    <div className="landingPageContainer">
+        <img className="landingBanner" src={bannerSvg} alt="banner" />
+      <h2 className="welcomeText boldText">Welcome to</h2>
+      <h1 className="boldText lifePlusText">life +</h1>
+      <h2 className="mediumText questionText" >Who are you?</h2>
+      <button className="btnPatient">Patient</button>
+      <button className="btnHp">Healthcare provider</button>
+      <p className="termsAcceptance">By signing  up or logging in, I accept the apps <em>Terms of service</em> and <em> Privacy policy</em></p>
     </div>
   )
 }
