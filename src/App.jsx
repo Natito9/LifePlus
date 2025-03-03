92caace606e2d22716bbb1cf405dad205d456733
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-03-03 12:21:34 +0100

    Add two fonts and content on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import LandingPage from './routes/shared/Login/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <p>Group 6</p>
-
+    <LandingPage />
     </div>
   );
 }
---
+++ src/routes/shared/Login/LandingPage/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LandingPage.css';
+import bannerSvg from "../../../../assets/images/banner.svg";
+
+function LandingPage() {
+  return (
+    <div>
+        <img src={bannerSvg} alt="banner" />
+      <h2 className='bold-text'>Welcome to</h2>
+      <h1 className='bold-text'>life +</h1>
+      <h2 className='medium-text'>Who are you?</h2>
+      <button>Patient</button>
+      <button>Healthcare provider</button>
+      <p>By signing  up or logging in, I accept the apps <em>Terms of service</em> and <em> Privacy policy</em></p>
+    </div>
+  )
+}
+
+export default LandingPage;
