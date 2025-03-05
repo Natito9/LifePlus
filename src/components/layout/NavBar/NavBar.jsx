671a1ecf988bb43b64021bfe1038b7899caa1249
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-03-05 09:51:33 +0100

    Modify navbar conditional rendering to always display, even when userRole is empty

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -6,43 +6,43 @@ import "./NavBar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const {userRole} = useUserRole();
+  const { userRole } = useUserRole();
 
   return (
     <nav className="navbar">
-      {userRole === "patient" ?(
+      {userRole === "patient" ? (
         <>
-      <button
-        className="btn homeNav"
-        onClick={()=> navigate("/patient/homepage")}
-      >
-        <img src={homeIcon} alt="Home Icon" className="homeNavIcon" />
-        Home
-      </button>
+          <button
+            className="btn homeNav"
+            onClick={() => navigate("/patient/homepage")}
+          >
+            <img src={homeIcon} alt="Home Icon" className="homeNavIcon" />
+            Home
+          </button>
 
-      <button className="btn moreNav">
-        {/* onclick open menu */}
-        <img src={moreIcon} alt="More Icon" className="moreNavIcon" />
-        More
-      </button>
-      </>): userRole === "provider" ? (
-        <>
-         <button
-        className="btn homeNav"
-        onClick={()=> navigate("/hp/homepage")}
-      >
-        <img src={homeIcon} alt="Home Icon" className="homeNavIcon" />
-        Home
-      </button>
-
-      <button className="btn moreNav">
-        {/* onclick open menu */}
-        <img src={moreIcon} alt="More Icon" className="moreNavIcon" />
-        More
-      </button>
+          <button className="btn moreNav">
+            {/* onclick open menu */}
+            <img src={moreIcon} alt="More Icon" className="moreNavIcon" />
+            More
+          </button>
         </>
-      ) : null
-      }
+      ) : (
+        <>
+          <button
+            className="btn homeNav"
+            onClick={() => navigate("/hp/homepage")}
+          >
+            <img src={homeIcon} alt="Home Icon" className="homeNavIcon" />
+            Home
+          </button>
+
+          <button className="btn moreNav">
+            {/* onclick open menu */}
+            <img src={moreIcon} alt="More Icon" className="moreNavIcon" />
+            More
+          </button>
+        </>
+      )}
     </nav>
   );
 };
