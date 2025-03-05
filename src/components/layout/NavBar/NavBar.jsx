dcd4d874c6307a36895b0d11c68131f0631b5a80
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-03-05 16:17:26 +0100

    Renames component to NavBar (same as the file name)

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -1,32 +1,12 @@
-/* import NavBarMobile from "./NavBarMobile";
-import NavBarDesktop from "./NavBarDesktop";
-import "./NavBar.css";
-
-function NavBar() {
-  return (
-    <>
-      <nav className="NavBar NavBarMobile">
-        <NavBarMobile />
-      </nav>
-      <nav className="NavBar NavBarDesktop">
-        <NavBarDesktop />
-      </nav>
-    </>
-  );
-}
-
-export default NavBar; */
-
-
 import useScreenSize from "../../../Hooks/useScreenSize";
 import NavBarMobile from "./NavBarMobile";
 import NavBarDesktop from "./NavBarDesktop";
 
-
-function HpHomepage() {
+// HpHomepage changed to NavBar
+function NavBar() {
    const isMobile = useScreenSize();
 
    return isMobile ? <NavBarMobile /> : <NavBarDesktop />;
 }
 
-export default HpHomepage;
+export default NavBar;
