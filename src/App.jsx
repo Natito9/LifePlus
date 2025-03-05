d1b228fe4153ba0c13a0429f0aa56d08e6f82a3d
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-03-05 09:41:43 +0100

    Creates doctor/ current patient profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./routes/shared/LandingPage/LandingPage";
 import Login from "./routes/shared/Login/Login";
